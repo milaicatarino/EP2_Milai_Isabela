6170af5e415f1f718e29b8bb50e41ccd39aa7b80
Isabella Leão <isabellallima23@gmail.com>
2022-05-13 10:01:08 -0300

    2 commit (Isabella)

--- Jogo_paises.py
+++ Jogo_paises.py
@@ -1,4 +1,5 @@
 import random as r
+import math
 
 # Funções necessárias
 # Normalizando bases de países
@@ -21,6 +22,16 @@ def sorteia_pais(dic):
     return pais
 
 # Distância
+def haversine(r, s_lat, s_lng, e_lat, e_lng):
+
+    s_lat = s_lat*math.pi/180                     
+    s_lng = s_lng*math.pi/180
+    e_lat = e_lat*math.pi/180                   
+    e_lng = e_lng*math.pi/180
+
+    d = 2 * r * math.asin((math.sin((e_lat - s_lat)/2)**2 + math.cos(s_lat)*math.cos(e_lat) * math.sin((e_lng - s_lng)/2)**2)**(1/2))
+
+    return d
 
 # Adicionando em uma lista
 def adiciona_em_ordem(pais,dist,l_paises):
@@ -44,3 +55,14 @@ def esta_na_lista(pais, l_paises):
         return False
 
 # Sorteia letra com restrição
+def sorteia_letra(palavra,lista):
+    p = palavra.lower()
+    saida = []
+    especial = ['.', ',', '-', ';', ' ']
+
+    for i in range(0,len(p)):
+        if p[i] not in lista:
+            if p[i] not in especial:
+                saida.append(p[i])
+              
+    return r.choice(saida)
